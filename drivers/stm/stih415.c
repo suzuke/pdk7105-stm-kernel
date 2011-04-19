0800b7cf93d35431ac570442cd1b46b688062267
Stuart Menefy <stuart.menefy@st.com>
2011-04-19 13:40:49 +0100

    sh_stm: Fix compilation warning
    drivers/stm/stih415.c:324: warning: passing argument 3 of 'stm_pad_init' from incompatible pointer type
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- drivers/stm/stih415.c
+++ drivers/stm/stih415.c
@@ -277,7 +277,7 @@ static struct platform_device stih415_pio_devices[] = {
 };
 
 static int stih415_pio_config(unsigned gpio,
-                enum stm_pad_gpio_direction direction, int function)
+                enum stm_pad_gpio_direction direction, int function, void* priv)
 {
 	switch (direction) {
 	case stm_pad_gpio_direction_in:
