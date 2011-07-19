672e574dc7d75c5241771456f5605020fcd39011
Stephen Gallimore <stephen.gallimore@st.com>
2011-07-19 19:17:40 +0100

    sh_stm: Add Mali memory configuration and platform initialization to the B2000 board
    Signed-off-by: Stephen Gallimore <stephen.gallimore@st.com>

--- arch/sh/boards/mach-b2000/setup.c
+++ arch/sh/boards/mach-b2000/setup.c
@@ -130,6 +130,29 @@ static struct platform_device *b2000_devices[] __initdata = {
 	&b2000_leds,
 };
 
+static struct stm_mali_resource b2000_mali_mem[1] = {
+	{
+		.name   = "OS_MEMORY",
+		.start  =  0,
+		.end    =  CONFIG_STM_B2000_MALI_OS_MEMORY_SIZE-1,
+	},
+};
+
+static struct stm_mali_resource b2000_mali_ext_mem[] = {
+	{
+		.name   = "EXTERNAL_MEMORY_RANGE",
+		.start  =  0x40000000,
+		.end    =  0xBFFFFFFF,
+	}
+};
+
+static struct stm_mali_config b2000_mali_config = {
+	.num_mem_resources = ARRAY_SIZE(b2000_mali_mem),
+	.mem = b2000_mali_mem,
+	.num_ext_resources = ARRAY_SIZE(b2000_mali_ext_mem),
+	.ext_mem = b2000_mali_ext_mem,
+};
+
 static int __init b2000_devices_init(void)
 {
 	/* Reset */
@@ -171,6 +194,8 @@ static int __init b2000_devices_init(void)
 	stih415_configure_usb(0);
 	stih415_configure_usb(1);
 	stih415_configure_usb(2);
+
+	stih415_configure_mali(&b2000_mali_config);
 
 	return platform_add_devices(b2000_devices,
 			ARRAY_SIZE(b2000_devices));
