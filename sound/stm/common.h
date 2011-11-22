f87704dbb14946d9e50f385c165017214ad19058
Kieran Bingham <kieran.bingham@st.com>
2011-11-22 17:29:03 +0000

    sound/stm: Include slab.h header to use kmalloc/kzalloc

--- sound/stm/common.h
+++ sound/stm/common.h
@@ -8,6 +8,7 @@
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/mm.h>
+#include <linux/slab.h>
 #include <linux/platform_device.h>
 #include <linux/stringify.h>
 #include <sound/core.h>
