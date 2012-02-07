2ce425bd7e630b2bb93d939f611e8b87cbc18d7f
Giuseppe CAVALLARO <peppe.cavallaro@st.com>
2012-02-07 10:51:18 +0100

    sh_stm: propate the sdhci clock for STx7108
    Signed-off-by: Giuseppe Cavallaro <peppe.cavallaro@st.com>

--- drivers/stm/stx7108_clock.c
+++ drivers/stm/stx7108_clock.c
@@ -30,8 +30,7 @@ int __init plat_clk_alias_init(void)
 	clk_add_alias("fdma_ic_clk", NULL, "CLKA_IC_REG_LP_ON", NULL);
 
 	/* SDHCI clocks */
-	clk_add_alias("sdhci_clk", NULL, "CLKA_SYS_MMC_SS",  NULL);
-	clk_add_alias("sdhci_card_clk", NULL, "CLKA_CARD_MMC_SS",  NULL);
+	clk_add_alias(NULL, "sdhci-stm.0", "CLKS_CARD_MMC",  NULL);
 
 	/* USB clocks */
 	clk_add_alias("usb_48_clk", NULL, "CLKB_FS1_CH3", NULL);
