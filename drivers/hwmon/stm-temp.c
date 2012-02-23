24261cb4f3aa5b6d7d58ae7798e7b2a604bb7a49
Francesco Virlinzi <francesco.virlinzi@st.com>
2012-02-23 08:33:11 +0100

    stm: temp: Update the pm_runtime support
    This patch updates the pm_runtime support to be really
    used on-demand.
    Now the sensor is "always" off and it just turned-on on the fly
    when the upper layer calls the 'stm_thermal_get_temp'
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>

--- drivers/hwmon/stm-temp.c
+++ drivers/hwmon/stm-temp.c
@@ -20,6 +20,7 @@
 #include <linux/stm/device.h>
 
 struct stm_temp_sensor {
+	struct platform_device *pdev;
 	struct stm_device_state *device_state;
 	struct thermal_zone_device *th_dev;
 	struct plat_stm_temp_data *plat_data;
@@ -39,6 +40,8 @@ static int stm_thermal_get_temp(struct thermal_zone_device *th,
 	unsigned long data;
 	int overflow;
 
+	pm_runtime_get_sync(&sensor->pdev->dev);
+
 	overflow = sysconf_read(sensor->overflow);
 
 	if (sensor->plat_data->custom_get_data)
@@ -53,26 +56,13 @@ static int stm_thermal_get_temp(struct thermal_zone_device *th,
 
 	*temperature = data;
 
+	pm_runtime_put(&sensor->pdev->dev);
+
 	return overflow;
-}
-
-static int stm_thermal_set_mode(struct thermal_zone_device *th,
-	enum thermal_device_mode val)
-{
-	struct stm_temp_sensor *sensor =
-		(struct stm_temp_sensor *)th->devdata;
-
-	if (val == THERMAL_DEVICE_DISABLED)
-		stm_device_power(sensor->device_state, stm_device_power_off);
-	else
-		stm_device_power(sensor->device_state, stm_device_power_on);
-
-	return 0;
 }
 
 static struct thermal_zone_device_ops stm_thermal_ops = {
 	.get_temp = stm_thermal_get_temp,
-	.set_mode = stm_thermal_set_mode,
 };
 
 static int __devinit stm_temp_probe(struct platform_device *pdev)
@@ -89,6 +79,7 @@ static int __devinit stm_temp_probe(struct platform_device *pdev)
 		goto error_kzalloc;
 	}
 
+	sensor->pdev = pdev;
 	sensor->plat_data = plat_data;
 
 	err = -EBUSY;
@@ -151,7 +142,6 @@ static int __devinit stm_temp_probe(struct platform_device *pdev)
 
 	/* Initialize the pm_runtime fields */
 	pm_runtime_set_active(&pdev->dev);
-	pm_suspend_ignore_children(&pdev->dev, 1);
 	pm_runtime_enable(&pdev->dev);
 
 	return 0;
@@ -198,7 +188,6 @@ static int stm_temp_suspend(struct device *dev)
 	if (dev->power.runtime_status != RPM_ACTIVE)
 		return 0; /* sensor already suspended via runtime_suspend */
 #endif
-
 	stm_device_power(sensor->device_state, stm_device_power_off);
 	return 0;
 }
@@ -209,42 +198,19 @@ static int stm_temp_resume(struct device *dev)
 
 #ifdef CONFIG_PM_RUNTIME
 	if (dev->power.runtime_status == RPM_SUSPENDED)
-		return 0; /* usb wants resume via runtime_resume... */
+		return 0; /* sensor wants resume via runtime_resume... */
 #endif
 	stm_device_power(sensor->device_state, stm_device_power_on);
 	return 0;
 }
-
-#ifdef CONFIG_PM_RUNTIME
-static int stm_temp_runtime_suspend(struct device *dev)
-{
-	if (dev->power.runtime_status == RPM_SUSPENDED)
-		return 0;
-
-	stm_temp_suspend(dev);
-	return 0;
-}
-
-static int stm_temp_runtime_resume(struct device *dev)
-{
-	if (dev->power.runtime_status == RPM_ACTIVE)
-		return 0;
-
-	stm_temp_resume(dev);
-	return 0;
-}
-#else
-#define stm_temp_runtime_suspend	NULL
-#define stm_temp_runtime_resume		NULL
-#endif
 
 static struct dev_pm_ops stm_temp_pm = {
 	.suspend = stm_temp_suspend,  /* on standby/memstandby */
 	.resume = stm_temp_resume,    /* resume from standby/memstandby */
 	.freeze = stm_temp_suspend,
 	.restore = stm_temp_resume,
-	.runtime_suspend = stm_temp_runtime_suspend,
-	.runtime_resume = stm_temp_runtime_resume,
+	.runtime_suspend = stm_temp_suspend,
+	.runtime_resume = stm_temp_resume,
 };
 #else
 static struct dev_pm_ops stm_temp_pm;
