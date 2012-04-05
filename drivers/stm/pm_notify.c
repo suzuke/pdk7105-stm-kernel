f6c92ddb2ac7d8a709f12f7c74b7ce56a0e84378
Francesco Virlinzi <francesco.virlinzi@st.com>
2012-04-05 10:50:48 +0200

    stm: pm: Update the woken-up reason early notify
    This patch updates the woken-up early notify.
    This is required to avoid all the system being totally woken-up for
    a wrong wake-up reason.
    
    With this API the system can perform early parsing
    of the wake-up reason (typically the infrared device driver).
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>

--- drivers/stm/pm_notify.c
+++ drivers/stm/pm_notify.c
@@ -2,49 +2,43 @@
  * -------------------------------------------------------------------------
  * <linux_root>/drivers/stm/pm_notify.c
  * -------------------------------------------------------------------------
- * Copyright (C) 2010  STMicroelectronics
+ * Copyright (C) 2012  STMicroelectronics
  * Author: Francesco M. Virlinzi  <francesco.virlinzi@st.com>
  *
  * May be copied or modified under the terms of the GNU General Public
  * License V.2 ONLY.  See linux/COPYING for more information.
  *
  * ------------------------------------------------------------------------- */
-#include <linux/irqflags.h>
+
 #include <linux/module.h>
 #include <linux/errno.h>
 #include <linux/mutex.h>
 #include <linux/stm/pm_notify.h>
 
 static DEFINE_MUTEX(pm_notify_mutex);
-static struct stm_pm_notify *stm_notifier;
+static LIST_HEAD(pm_notify_list);
 
-enum stm_pm_notify_return
-stm_pm_prepare_enter(enum stm_pm_type type)
+enum stm_pm_notify_ret stm_pm_early_check(int wkirq)
 {
-	if (stm_notifier && stm_notifier->pre_enter)
-		return stm_notifier->pre_enter(type);
+	struct stm_pm_notify *handler;
+
+	list_for_each_entry(handler, &pm_notify_list, list)
+		if (handler->irq == wkirq)
+			return handler->notify();
 
 	return STM_PM_RET_OK;
 }
 
-enum stm_pm_notify_return
-stm_pm_post_enter(enum stm_pm_type type, int wkirq)
+int stm_register_pm_notify(struct stm_pm_notify *handler)
 {
-	if (stm_notifier && stm_notifier->post_enter)
-		return stm_notifier->post_enter(type, wkirq);
-
-	return STM_PM_RET_OK;
-}
-
-int stm_register_pm_notify(struct stm_pm_notify *notify)
-{
-	if (stm_notifier || !notify)
+	if (!handler || !handler->notify)
 		return -EINVAL;
 
 	mutex_lock(&pm_notify_mutex);
-	stm_notifier = notify;
+	list_add(&handler->list, &pm_notify_list);
 	mutex_unlock(&pm_notify_mutex);
 
 	return 0;
 }
+
 EXPORT_SYMBOL(stm_register_pm_notify);
--- include/linux/stm/pm_notify.h
+++ include/linux/stm/pm_notify.h
@@ -4,7 +4,7 @@
  * -------------------------------------------------------------------------
  * STMicroelectronics
  * -------------------------------------------------------------------------
- * Copyright (C) 2010  STMicroelectronics
+ * Copyright (C) 2012  STMicroelectronics
  * Author: Francesco M. Virlinzi  <francesco.virlinzi@st.com>
  *
  * May be copied or modified under the terms of the GNU General Public
@@ -15,32 +15,20 @@
 #ifndef __STM_LINUX_NOTIFY__
 #define __STM_LINUX_NOTIFY__
 
-enum stm_pm_type {
-	STM_PM_SUSPEND,
-	STM_PM_MEMSUSPEND,
-	STM_PM_HIBERNATION,
-};
+#include <linux/list.h>
 
-enum stm_pm_notify_return {
+enum stm_pm_notify_ret {
 	STM_PM_RET_OK,
-	STM_PM_RET_ERROR,
 	STM_PM_RET_AGAIN,
 };
 
 struct stm_pm_notify {
-
-	enum stm_pm_notify_return (*pre_enter)
-		(enum stm_pm_type type);
-
-	enum stm_pm_notify_return (*post_enter)
-		(enum stm_pm_type type, int wkirq);
+	struct list_head list;
+	int irq;
+	enum stm_pm_notify_ret (*notify)(void);
 };
 
-enum stm_pm_notify_return
-stm_pm_prepare_enter(enum stm_pm_type type);
-
-enum stm_pm_notify_return
-stm_pm_post_enter(enum stm_pm_type type, int wkirq);
+enum stm_pm_notify_ret stm_pm_early_check(int irq_reason);
 
 int stm_register_pm_notify(struct stm_pm_notify *notify);
 
