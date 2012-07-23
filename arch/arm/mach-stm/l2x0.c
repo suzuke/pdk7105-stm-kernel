8be4faae45a0de9269eabb62f580d3c2eaafae0d
Srinivas KANDAGATLA <srinivas.kandagatla@st.com>
2012-07-23 14:44:56 +0100

    stm:ARM:l2x0: Optimise L2 cache latency on MPE41
    Add some optimizations to TAG, DATA RAM latencies in L2 cache, On MPE41.
    By default the latencies are 8,8,8 cycles for setup, read, write, with
    this patch these values are changed (in number of clock cycles):
    
        Tag RAM latencies: write / read / setup: 1 / 1 / 1
        Data RAM latencies: write / read / setup: 2 / 3 / 2
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@st.com>

--- arch/arm/mach-stm/l2x0.c
+++ arch/arm/mach-stm/l2x0.c
@@ -21,6 +21,33 @@ static int __init mpe41_l2x0_init(void)
 {
 #ifdef CONFIG_CACHE_L2X0
 	void __iomem *base = __io_address(MPE41_PL310_BASE);
+	u32 val;
+	/*
+	* Tag RAM Control register
+	*
+	* bit[10:8]    - 1 cycle of write accesses latency
+	* bit[6:4]     - 1 cycle of read accesses latency
+	* bit[2:0]     - 1 cycle of setup latency
+	*
+	* 1 cycle of latency for setup, read and write accesses
+	*/
+	val = readl(base + L2X0_TAG_LATENCY_CTRL);
+	val &= 0xfffff888;
+	writel(val, base + L2X0_TAG_LATENCY_CTRL);
+
+	/*
+	* Data RAM Control register
+	*
+	* bit[10:8]    - 2 cycles of write accesses latency
+	* bit[6:4]     - 3 cycles of read accesses latency
+	* bit[2:0]     - 2 cycle of setup latency
+	*
+	* 1 cycle of latency for setup, read and write accesses
+	*/
+	val = readl(base + L2X0_DATA_LATENCY_CTRL);
+	val &= 0xfffff888;
+	val |= 0xfffff9a9;
+	writel(val, base + L2X0_DATA_LATENCY_CTRL);
 
 	/* We have to ensure that bit 22 is set. This bit controls if
 	 * shared uncacheable normal memory accesses are looked up in the cache
