e63ea37c6f3ad4e28eff805582c1674a2862a56c
John Boddie <john.boddie@st.com>
2012-10-17 14:35:11 +0000

    stm:snd:stih415 Add power management support to ALSA glue driver
    This patch adds power management support to the STiH415 ALSA glue driver.
    To do this the code has changed from just a loadable module to an actual
    platform driver allowing the power management callbacks to be added.
    
    The power management callbacks are required as the sysconf registers do
    not retain there value after HoM.
    
    Signed-off-by: John Boddie <john.boddie@st.com>

--- sound/stm/stih415.c
+++ sound/stm/stih415.c
@@ -57,66 +57,99 @@ static struct sysconf_field *snd_stm_stih415_pcm_clk_sel;
 static struct sysconf_field *snd_stm_stih415_biphase_idle_value;
 static struct sysconf_field *snd_stm_stih415_voip;
 static struct sysconf_field *snd_stm_stih415_pcmp_valid_sel;
+static struct snd_info_entry *snd_stm_stih415_proc_entry;
 
-static int __init snd_stm_stih415_init(void)
+static void snd_stm_stih415_procfs(struct snd_info_entry *entry,
+		struct snd_info_buffer *buffer)
+{
+	snd_iprintf(buffer, "pcm_clk_sel        = %08lx\n",
+			sysconf_read(snd_stm_stih415_pcm_clk_sel));
+	snd_iprintf(buffer, "biphase_idle_value = %08lx\n",
+			sysconf_read(snd_stm_stih415_biphase_idle_value));
+	snd_iprintf(buffer, "voip               = %08lx\n",
+			sysconf_read(snd_stm_stih415_voip));
+	snd_iprintf(buffer, "pcmp_valid_sel     = %08lx\n",
+			sysconf_read(snd_stm_stih415_pcmp_valid_sel));
+}
+
+static void snd_stm_stih415_setup(void)
+{
+	/* Select external pcm clock for each channel */
+	sysconf_write(snd_stm_stih415_pcm_clk_sel, 0xf);
+	/* Set bi-phase idle value */
+	sysconf_write(snd_stm_stih415_biphase_idle_value, 0);
+	/* Clear all voip bits for now */
+	sysconf_write(snd_stm_stih415_voip, 0);
+	/* Route pcm players */
+	sysconf_write(snd_stm_stih415_pcmp_valid_sel, 1);
+}
+
+static int __init snd_stm_stih415_probe(struct platform_device *pdev)
 {
 	int result;
 
-	snd_stm_printd(0, "%s()\n", __func__);
+	dev_dbg(&pdev->dev, "%s()", __func__);
 
 	if (!stm_soc_is_stih415()) {
-		snd_stm_printe("Unsupported (not STiH415) SOC detected!\n");
-		result = -EINVAL;
-		goto error_soc_type;
+		dev_err(&pdev->dev, "Unsupported (not STiH415) SOC detected!");
+		return -EINVAL;
 	}
 
-	/* Select external pcm clock for each channel */
+	/* Claim external pcm clock sysconf */
 	snd_stm_stih415_pcm_clk_sel = sysconf_claim(SYSCONF(331), 8, 11,
 						       "PCM_CLK_SEL");
 	if (!snd_stm_stih415_pcm_clk_sel) {
-		snd_stm_printe("Failed to claim sysconf (PCM_CLK_SEL)\n");
-		result = -EBUSY;
-		goto error_sysconf_claim_pcm_clk_sel;
+		dev_err(&pdev->dev, "Failed to claim PCM_CLK_SEL");
+		return -EBUSY;
 	}
-	sysconf_write(snd_stm_stih415_pcm_clk_sel, 0xf);
 
-	/* Set bi-phase idle value */
+	/* Claim bi-phase idle value sysconf */
 	snd_stm_stih415_biphase_idle_value = sysconf_claim(SYSCONF(331), 7, 7,
 						       "BIPHASE_IDLE_VALUE");
 	if (!snd_stm_stih415_biphase_idle_value) {
-		snd_stm_printe("Failed to claim sysconf (BIPHASE_IDLE_VALUE)\n");
+		dev_err(&pdev->dev, "Failed to claim BIPHASE_IDLE_VALUE");
 		result = -EBUSY;
 		goto error_sysconf_claim_biphase_idle_value;
 	}
-	sysconf_write(snd_stm_stih415_biphase_idle_value, 0);
 
-	/* Clear all voip bits for now */
+	/* Claim voip sysconf */
 	snd_stm_stih415_voip = sysconf_claim(SYSCONF(331), 2, 5, "VOIP");
 	if (!snd_stm_stih415_voip) {
-		snd_stm_printe("Failed to claim sysconf (VOIP)\n");
+		dev_err(&pdev->dev, "Failed to claim VOIP");
 		result = -EBUSY;
 		goto error_sysconf_claim_voip;
 	}
-	sysconf_write(snd_stm_stih415_voip, 0);
 
-	/* Route pcm players */
+	/* Claim pcm player routing sysconf */
 	snd_stm_stih415_pcmp_valid_sel = sysconf_claim(SYSCONF(331), 0, 1,
 						       "PCMP_VALID_SEL");
 	if (!snd_stm_stih415_pcmp_valid_sel) {
-		snd_stm_printe("Failed to claim sysconf (PCMP_VALID_SEL)\n");
+		dev_err(&pdev->dev, "Failed to claim PCMP_VALID_SEL");
 		result = -EBUSY;
 		goto error_sysconf_claim_pcmp_valid_sel;
 	}
-	sysconf_write(snd_stm_stih415_pcmp_valid_sel, 1);
 
+	/* Set the sysconf values */
+	snd_stm_stih415_setup();
+
+	/* Register the sound card */
 	result = snd_stm_card_register();
-	if (result != 0) {
-		snd_stm_printe("Failed to register ALSA cards!\n");
+	if (result) {
+		dev_err(&pdev->dev, "Failed to register ALSA cards!");
 		goto error_card_register;
+	}
+
+	/* Register a procfs file */
+	result = snd_stm_info_register(&snd_stm_stih415_proc_entry,
+			dev_name(&pdev->dev), snd_stm_stih415_procfs, NULL);
+	if (result) {
+		dev_err(&pdev->dev, "Failed to register with procfs");
+		goto error_info_register;
 	}
 
 	return 0;
 
+error_info_register:
 error_card_register:
 	sysconf_release(snd_stm_stih415_pcmp_valid_sel);
 error_sysconf_claim_pcmp_valid_sel:
@@ -125,22 +158,88 @@ error_sysconf_claim_voip:
 	sysconf_release(snd_stm_stih415_biphase_idle_value);
 error_sysconf_claim_biphase_idle_value:
 	sysconf_release(snd_stm_stih415_pcm_clk_sel);
-error_sysconf_claim_pcm_clk_sel:
-error_soc_type:
 	return result;
 }
 
-static void __exit snd_stm_stih415_exit(void)
+static int __devexit snd_stm_stih415_remove(struct platform_device *pdev)
 {
-	snd_stm_printd(0, "%s()\n", __func__);
+	dev_dbg(&pdev->dev, "%s()", __func__);
 
 	sysconf_release(snd_stm_stih415_pcmp_valid_sel);
 	sysconf_release(snd_stm_stih415_voip);
 	sysconf_release(snd_stm_stih415_biphase_idle_value);
 	sysconf_release(snd_stm_stih415_pcm_clk_sel);
+
+	return 0;
 }
 
-MODULE_AUTHOR("Sevanand Singh <sevanand.singh@st.com>");
+
+#ifdef CONFIG_PM
+static int snd_stm_stih415_suspend(struct device *dev)
+{
+	dev_dbg(dev, "%s()", __func__);
+	return 0;
+}
+
+static int snd_stm_stih415_resume(struct device *dev)
+{
+	dev_dbg(dev, "%s()", __func__);
+
+	/* Re-write the sysconf registers */
+	snd_stm_stih415_setup();
+
+	return 0;
+}
+
+static const struct dev_pm_ops snd_stm_stih415_pm_ops = {
+	.suspend = snd_stm_stih415_suspend,
+	.resume	 = snd_stm_stih415_resume,
+	.freeze	 = snd_stm_stih415_suspend,
+	.restore = snd_stm_stih415_resume,
+};
+#endif
+
+
+static struct platform_driver snd_stm_stih415_driver = {
+	.driver.name	= "snd_stih415",
+#ifdef CONFIG_PM
+	.driver.pm	= &snd_stm_stih415_pm_ops,
+#endif
+	.probe		= snd_stm_stih415_probe,
+	.remove		= snd_stm_stih415_remove,
+};
+
+
+/*
+ * Module initialistaion.
+ */
+
+static struct platform_device *snd_stm_stih415_devices[] __initdata = {
+	&(struct platform_device) {
+		.name = "snd_stih415",
+		.id = -1,
+	},
+};
+
+static int __init snd_stm_stih415_init(void)
+{
+	int result;
+
+	/* Add the STiH415 audio glue platform device */
+	result = platform_add_devices(snd_stm_stih415_devices, 1);
+	BUG_ON(result);
+
+	/* Register the platform driver */
+	return platform_driver_register(&snd_stm_stih415_driver);
+}
+
+static void __exit snd_stm_stih415_exit(void)
+{
+	platform_driver_unregister(&snd_stm_stih415_driver);
+}
+
+
+MODULE_AUTHOR("John Boddie <john.boddie@st.com>");
 MODULE_DESCRIPTION("STMicroelectronics STiH415 audio glue driver");
 MODULE_LICENSE("GPL");
 
