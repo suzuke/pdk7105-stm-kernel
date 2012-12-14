268e155fc09821fb1fed2fae740976ceb23a3fc2
Francesco VIRLINZI <francesco.virlinzi@st.com>
2012-12-14 14:47:50 +0000

    stm: arm: Define the correct NR_IRQS for each chip
    This patch defines the correct NR_IRQS for each supported chip.
    Moreover a compilation error is raised when a new chip will be added
    without the related NR_GPIO_PORTS definition.
    
    Signed-off-by: Francesco Virlinzi <francesco.virlinzi@st.com>

--- arch/arm/mach-stm/include/mach/irqs.h
+++ arch/arm/mach-stm/include/mach/irqs.h
@@ -12,15 +12,23 @@
 #ifndef __ASM_ARCH_IRQS_H
 #define __ASM_ARCH_IRQS_H
 
-#include <mach/irqs-stih415.h>
-
 #define IRQ_GLOBALTIMER		27
 #define IRQ_LOCALTIMER		29
 #define IRQ_LOCALWDOG		30
 
 #define IRQ_GIC_START		32
 
-#define NR_PIO_IRQS	(8*27)
+#if defined(CONFIG_MACH_STM_STIH415)
+# define NR_GPIO_PORTS		27
+#elif defined(CONFIG_MACH_STM_FLI7610)
+# define NR_GPIO_PORTS		34
+#elif defined(CONFIG_MACH_STM_STIG125)
+# define NR_GPIO_PORTS		27
+#else
+# error	NR_GPIO_PORTS not defined for this chip
+#endif
+
+#define NR_PIO_IRQS	(8 * NR_GPIO_PORTS)
 #define NR_MSI_IRQS	32
 #define NR_IRQS		(IRQ_GIC_START + 224 + NR_PIO_IRQS + NR_MSI_IRQS)
 
