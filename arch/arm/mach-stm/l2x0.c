25b229788dc06b1024bd657498a04356278ae9c1
Carmelo AMOROSO <carmelo.amoroso@st.com>
2012-12-14 14:47:53 +0000

    stm: arm: l2x0: add support for stih416 SoC
    STiH416 needs some special configuration for TAG and DATA latency as below:
    
    ------------------------------------------------------------
                        |  write   |   read   |  setup   | bits
                        | # cycles | # cycles | # cycles |
    ------------------------------------------------------------
    TAG latency cycles  |    3     |    3     |     3    | 0x222
    DATA latency cycles |    2     |    2     |     2    | 0x111
    -------------------------------------------------------------
    
    Further, as the reset value of the aux ctrl register is 0x200000 (due to
    a different set in the RTL of L2$ for Orly-2), it implies that bits[19:17] (way side)
    are zero, so it's mandatory to explicitely set the way size by setting bits [19:17]
    accordingly. The mask for writing the aux ctrl register must be includes
    also the bits for the way size.
    
    (For other chips, reset value is 0x208000, so the way size is correctly set)
    Anyway, it's absolutely safe to force this writing for all the chips, without
    relying upon the reset value of the control register.
    
    To make code more readable, prefer using meaningful macros, instead of plain
    numeric constants.
    
    This patch also fixes the way size for SASC-1, that is 64KB instead of
    128KB.
    
    Suggested-by: Aymen Bouattay <aymen.bouattay@st.com>
    Signed-off-by: Carmelo Amoroso <carmelo.amoroso@st.com>

--- arch/arm/mach-stm/l2x0.c
+++ arch/arm/mach-stm/l2x0.c
@@ -1,5 +1,5 @@
 /*
- * L2 cache initialization for MPE41
+ * L2 cache initialization for MPE41 and MPE42
  *
  * Copyright (C) 2012 STMicroelectronics Limited.
  * Author: Srinivas Kandagatla <srinivas.kandagatla@st.com>
@@ -16,48 +16,68 @@
 #include <linux/stm/soc.h>
 #include <mach/hardware.h>
 #include <mach/mpe41.h>
+#include <mach/mpe42.h>
 #include <mach/soc-stig125.h>
 #include <asm/hardware/cache-l2x0.h>
+
+#define STM_L2X0_AUX_CTRL_MASK \
+	(0xffffffff & ~(L2X0_AUX_CTRL_WAY_SIZE_MASK | \
+			L2X0_AUX_CTRL_SHARE_OVERRIDE_MASK | \
+			L2X0_AUX_CTRL_PREFETCH_MASK))
 
 static int __init stm_l2x0_init(void)
 {
 #ifdef CONFIG_CACHE_L2X0
 	void __iomem *base;
-	u32 val;
+	u32 val, aux_ctrl;
+	/* Default value for almost all chips */
+	/* 1 cycle of latency for setup, read and write accesses */
+	u32 tag_latency  = 0;
+	/* 2, 3, 2 cycles of latency for setup, read and write accesses */
+	u32 data_latency = 0x121;
+	/* 128 KB */
+	u32 way_size = 0x4;
 
 	if (stm_soc_is_fli7610() || stm_soc_is_stih415())
 		base = __io_address(MPE41_PL310_BASE);
-	else if (stm_soc_is_stig125())
+	else if (stm_soc_is_stig125()) {
 		base = __io_address(STIG125_PL310_BASE);
+		/* 64 KB */
+		way_size = 0x3;
+	} else if (stm_soc_is_stih416()) {
+		base = __io_address(MPE42_PL310_BASE);
+		/* 3 cycles of latency for setup, read and write accesses */
+		data_latency = 0x222;
+		/* 2 cycles of latency for setup, read and write accesses */
+		tag_latency = 0x111;
+	}
 	else
 		return -ENOSYS;
 
 	/*
 	* Tag RAM Control register
 	*
-	* bit[10:8]    - 1 cycle of write accesses latency
-	* bit[6:4]     - 1 cycle of read accesses latency
-	* bit[2:0]     - 1 cycle of setup latency
+	* bit[10:8]    - # cycles of write accesses latency
+	* bit[6:4]     - # cycles of read accesses latency
+	* bit[2:0]     - # cycles of setup latency
 	*
-	* 1 cycle of latency for setup, read and write accesses
 	*/
 	val = readl(base + L2X0_TAG_LATENCY_CTRL);
 	val &= 0xfffff888;
+	val |= tag_latency;
 	writel(val, base + L2X0_TAG_LATENCY_CTRL);
 
 	/*
 	* Data RAM Control register
 	*
-	* bit[10:8]    - 2 cycles of write accesses latency
-	* bit[6:4]     - 3 cycles of read accesses latency
-	* bit[2:0]     - 2 cycle of setup latency
+	* bit[10:8]    - # cycles of write accesses latency
+	* bit[6:4]     - # cycles of read accesses latency
+	* bit[2:0]     - # cycles of setup latency
 	*
-	* 2, 3, 3 cycle of latency for setup, read and write accesses
-	* respectively.
 	*/
 	val = readl(base + L2X0_DATA_LATENCY_CTRL);
 	val &= 0xfffff888;
-	val |= 0x121;
+	val |= data_latency;
 	writel(val, base + L2X0_DATA_LATENCY_CTRL);
 
 	/* We have to ensure that bit 22 is set. This bit controls if
@@ -71,7 +91,12 @@ static int __init stm_l2x0_init(void)
 	 *
 	 * Instruction and Data prefetching now enabled (bit 28 and 29)
 	 */
-	l2x0_init(base, 0x1<<28 | 0x1<<29 | 0x1<<22, 0xcfbfffff);
+	aux_ctrl = (0x1 << L2X0_AUX_CTRL_SHARE_OVERRIDE_SHIFT) |
+		(0x1 << L2X0_AUX_CTRL_DATA_PREFETCH_SHIFT) |
+		(0x1 << L2X0_AUX_CTRL_INSTR_PREFETCH_SHIFT) |
+		(way_size << L2X0_AUX_CTRL_WAY_SIZE_SHIFT);
+
+	l2x0_init(base, aux_ctrl, STM_L2X0_AUX_CTRL_MASK);
 #endif
 	return 0;
 }
