c253377732f2716e0cc5797f5c1d78b599f6bd90
Stuart Menefy <stuart.menefy@st.com>
2013-03-21 12:36:53 +0000

    ata: ahci_stm: Simplify ahci device creation
    The creation of the underlying "ahci" platform device can be simplified
    by using devm_...() functions where possible, and
    platform_device_register_full() to create the device with all its
    attributes in one go.
    
    Use module_platform_driver() to create the register/unregister functions.
    
    Ensure all symbols use consistent naming (ahci_stm_... and drv_data).
    
    Add some missing error checking when claiming resources, and ensure
    resources are freed in the case of error and when the device
    is removed.
    
    Remove some unused code, in particular there is no point in specifying
    a struct ahci_platform_data in the struct stm_plat_ahci_data as the
    user would have no way to set the callback functions.
    
    Signed-off-by: Stuart Menefy <stuart.menefy@st.com>

--- drivers/ata/ahci_stm.c
+++ drivers/ata/ahci_stm.c
@@ -22,102 +22,93 @@
 struct ahci_stm_drv_data {
 	struct clk *clk;
 	struct stm_device_state *device_state;
-	struct platform_device *pdev;
 	struct platform_device *ahci;
+	struct stm_miphy *miphy;
 };
 
-static int stm_ahci_init(struct device *dev, void __iomem *mmio)
+static int ahci_stm_init(struct device *dev, void __iomem *mmio)
 {
-	struct platform_device *pdev = to_platform_device(dev);
-
 	writel(0x80000000, mmio + AHCI_OOBR);
 	writel(0x8204080C, mmio + AHCI_OOBR);
 	writel(0x0204080C, mmio + AHCI_OOBR);
 
-	stm_miphy_claim(pdata->miphy_num, SATA_MODE, ahci_dev);
-
 	return 0;
 };
 
-static struct ahci_platform_data default_ahci_platform_data = {
-	.init = stm_ahci_init,
+static struct ahci_platform_data ahci_stm_platform_data = {
+	.init = ahci_stm_init,
 };
-
-static struct platform_device
-__init *prepare_ahci(struct platform_device *pdev)
-{
-	struct stm_plat_ahci_data *pdata = dev_get_platdata(&pdev->dev);
-	struct platform_device *ahci;
-	struct ahci_platform_data *ahci_pdata;
-
-	ahci = platform_device_alloc("ahci", pdev->id);
-
-	platform_device_add_resources(ahci, pdev->resource, 2);
-
-	ahci_pdata = (pdata->ahci_pdata ? : &default_ahci_platform_data);
-	platform_device_add_data(ahci, ahci_pdata, sizeof(*ahci_pdata));
-
-	ahci->dev.parent = &pdev->dev;
-	ahci->dev.dma_mask = pdev->dev.dma_mask;
-	ahci->dev.coherent_dma_mask = pdev->dev.coherent_dma_mask;
-
-	return ahci;
-}
 
 static int __devinit ahci_stm_driver_probe(struct platform_device *pdev)
 {
 	struct stm_plat_ahci_data *pdata = dev_get_platdata(&pdev->dev);
 	struct ahci_stm_drv_data *drv_data;
 	struct device *dev = &pdev->dev;
-	int ret = 0;
+	struct platform_device_info ahci_info = {
+		.parent = dev,
+		.name = "ahci",
+		.id = pdev->id,
+		.res = pdev->resource,
+		.num_res = pdev->num_resources,
+		.data = &ahci_stm_platform_data,
+		.size_data = sizeof(ahci_stm_platform_data),
+		.dma_mask = *dev->dma_mask,
+	};
+	int ret;
 
-	drv_data = kzalloc(sizeof(*drv_data), GFP_KERNEL);
+	drv_data = devm_kzalloc(dev, sizeof(*drv_data), GFP_KERNEL);
 	if (!drv_data)
 		return -ENOMEM;
 
-	drv_data->pdev = pdev;
-	drv_data->clk = clk_get(dev, "ahci_clk");
-	if (IS_ERR(drv_data->clk)) {
-		ret = -EINVAL;
-		goto err_0;
-	}
+	drv_data->clk = devm_clk_get(dev, "ahci_clk");
+	if (IS_ERR(drv_data->clk))
+		return PTR_ERR(drv_data->clk);
 
 	pm_clk_init(dev);
 	pm_clk_add(dev, "ahci_clk");
-	clk_prepare_enable(drv_data->clk);
+
+	ret = clk_prepare_enable(drv_data->clk);
+	if (ret)
+		goto fail_pm_cleanup;
 
 	drv_data->device_state =
 		devm_stm_device_init(&pdev->dev, pdata->device_config);
 	if (!drv_data->device_state) {
 		ret = -EBUSY;
-		goto err_1;
+		goto fail_clk_disable;
 	}
+
+	drv_data->miphy = stm_miphy_claim(pdata->miphy_num, SATA_MODE, dev);
+	if (!drv_data->miphy) {
+		ret = -EBUSY;
+		goto fail_clk_disable;
+	}
+
 	platform_set_drvdata(pdev, drv_data);
 
-	drv_data->ahci = prepare_ahci(pdev);
-	if (!drv_data->ahci) {
-		ret = -ENODEV;
-		goto err_1;
+	drv_data->ahci = platform_device_register_full(&ahci_info);
+	if (IS_ERR(drv_data->ahci)) {
+		ret = PTR_ERR(drv_data->ahci);
+		goto fail_free_miphy;
 	}
-	ret = platform_device_add(drv_data->ahci);
-	if (ret)
-		goto err_1;
 
 	return 0;
-err_1:
+
+fail_free_miphy:
+	stm_miphy_release(drv_data->miphy);
+fail_clk_disable:
+	clk_disable_unprepare(drv_data->clk);
+fail_pm_cleanup:
 	pm_clk_destroy(&pdev->dev);
-err_0:
-	kfree(drv_data);
-	pr_err("Error: %s: %d\n", __func__, ret);
 	return ret;
 }
 
 #ifdef CONFIG_PM
 static int ahci_stm_suspend(struct device *dev)
 {
-	struct ahci_stm_drv_data *data = dev_get_drvdata(dev);
+	struct ahci_stm_drv_data *drv_data = dev_get_drvdata(dev);
 
-	stm_device_power(data->device_state, stm_device_power_off);
+	stm_device_power(drv_data->device_state, stm_device_power_off);
 	pm_clk_suspend(dev);
 
 	return 0;
@@ -125,10 +116,10 @@ static int ahci_stm_suspend(struct device *dev)
 
 static int ahci_stm_resume(struct device *dev)
 {
-	struct ahci_stm_drv_data *data = dev_get_drvdata(dev);
+	struct ahci_stm_drv_data *drv_data = dev_get_drvdata(dev);
 
 	pm_clk_resume(dev);
-	stm_device_power(data->device_state, stm_device_power_on);
+	stm_device_power(drv_data->device_state, stm_device_power_on);
 
 	return 0;
 }
@@ -138,11 +129,15 @@ static SIMPLE_DEV_PM_OPS(ahci_stm_pm_ops, ahci_stm_suspend, ahci_stm_resume);
 
 static int ahci_stm_remove(struct platform_device *pdev)
 {
-	struct ahci_stm_drv_data *data = dev_get_drvdata(&pdev->dev);
+	struct ahci_stm_drv_data *drv_data = dev_get_drvdata(&pdev->dev);
+
+	stm_miphy_release(drv_data->miphy);
 
 	pm_clk_destroy(&pdev->dev);
 
-	stm_device_power(data->device_state, stm_device_power_off);
+	stm_device_power(drv_data->device_state, stm_device_power_off);
+
+	platform_device_unregister(drv_data->ahci);
 
 	return 0;
 }
@@ -155,8 +150,4 @@ static struct platform_driver ahci_stm_driver = {
 	.remove = ahci_stm_remove,
 };
 
-static int __init ahci_stm_init(void)
-{
-	return platform_driver_register(&ahci_stm_driver);
-}
-module_init(ahci_stm_init);
+module_platform_driver(ahci_stm_driver);
--- include/linux/stm/platform.h
+++ include/linux/stm/platform.h
@@ -317,10 +317,6 @@ struct stm_plat_sata_data {
 
 struct stm_plat_ahci_data {
 	struct stm_device_config *device_config;
-	/* the driver has a default ahci_pdata
-	 * therefore the ahci_pdata can be NULL on several chips
-	 */
-	struct ahci_platform_data *ahci_pdata;
 	int miphy_num;
 };
 /*** PIO platform data ***/
