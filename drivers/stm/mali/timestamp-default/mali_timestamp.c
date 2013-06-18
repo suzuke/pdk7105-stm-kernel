fcd761999e625e578e594ecb39475cca7651aeb8
Angelo CASTELLO <angelo.castello@st.com>
2013-06-18 13:42:13 +0000

    stm:mali: driver updated to r3p2-01rel1 ARM GPLed version.
    This came from the DX910-SW-99002-r3p2-01rel1.tgz package downloaded from:
    http://www.malideveloper.com/developer-resources/drivers/index.php
    Extracted and used only the mali device driver scheme:
    DX910-SW-99002-r3p2-01rel1/driver/src/devicedrv/mali/
    
    Signed-off-by: Angelo Castello <angelo.castello@st.com>

--- drivers/stm/mali/timestamp-default/mali_timestamp.c
+++ drivers/stm/mali/timestamp-default/mali_timestamp.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010, 2012 ARM Limited. All rights reserved.
+ * Copyright (C) 2010-2011, 2013 ARM Limited. All rights reserved.
  * 
  * This program is free software and is provided to you under the terms of the GNU General Public License version 2
  * as published by the Free Software Foundation, and any use by you of this program is subject to the terms of such GNU licence.
@@ -8,10 +8,6 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-#ifndef __MALI_KERNEL_PM_H__
-#define __MALI_KERNEL_PM_H__
+#include "mali_timestamp.h"
 
-int _mali_dev_platform_register(void);
-void _mali_dev_platform_unregister(void);
-
-#endif /* __MALI_KERNEL_PM_H__ */
+/* This file is intentionally left empty, as all functions are inlined in mali_profiling_sampler.h */
