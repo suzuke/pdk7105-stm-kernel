73892d6f483d7315fd903350f2e96a29b6ecdd87
Udit KUMAR <udit-dlh.kumar@st.com>
2013-07-17 11:08:18 +0000

    stm: lpm: remove power pio handling for b2020_h416
    This patch removes power pio handling from board file,
    As discussed  in bug 33419, better place to handle power pio is
    lpm driver.
    
    Signed-off-by: Udit Kumar <udit-dlh.kumar@st.com>

--- arch/arm/mach-stm/board-b2020_h416.c
+++ arch/arm/mach-stm/board-b2020_h416.c
@@ -71,23 +71,8 @@ static struct stm_pad_config stih416_hdmi_hp_pad_config = {
 	},
 };
 
-static b2020_power_on_gpio = -EINVAL;
 static void __init b2020_dt_init(void)
 {
-	int power_on_gpio;
-	struct device_node *np = of_find_node_by_path("/soc");
-	if (np) {
-		power_on_gpio = of_get_named_gpio(np, "power-on-gpio", 0);
-		if (power_on_gpio > 0) {
-			gpio_request(power_on_gpio, "POWER_PIO");
-			gpio_direction_output(power_on_gpio, 1);
-			b2020_power_on_gpio = power_on_gpio;
-		}
-		of_node_put(np);
-	} else {
-		WARN_ON(!np);
-	}
-
 	of_platform_populate(NULL, of_default_bus_match_table,
 				 stih416_auxdata_lookup, NULL);
 	/* Default to HDMI HotPlug */
@@ -149,15 +134,8 @@ MACHINE_END
 
 #include <linux/stm/hom.h>
 
-static int b2020_hom_restore(struct stm_wakeup_devices *dev_wk)
-{
-	gpio_direction_output(b2020_power_on_gpio, 1);
-	return 0;
-}
-
 static struct stm_hom_board b2020_hom = {
 	.lmi_retention_gpio = stih416_gpio(4, 4),
-	.restore = b2020_hom_restore,
 };
 
 static int __init b2020_hom_init(void)
